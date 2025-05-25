f01c5776783228cda2bc328035f9eec33b78cce0
wawaZIMBAWAWA <Den.korolev2002@mail.ru>
2025-05-25 21:00:24 +0300

    Add files via upload

---
+++ Mass.java
@@ -0,0 +1,12 @@
+public class Mass {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Чётные числа в массиве:");
+        for (int number : numbers) {
+            if (number % 2 == 0) { 
+                System.out.println(number);
+            }
+        }
+    }
+}
